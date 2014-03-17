4469b75e4b6111f260bca9f5ce8c35e7d7e165f3
New Front <niels@newfront.eu>
2014-03-17 22:08:32 +0100

    Display Meetup title + description + RSVP information

--- app/js/pages/meetup-detail/meetup-detail.js
+++ app/js/pages/meetup-detail/meetup-detail.js
@@ -1,5 +1,17 @@
 angular.module('ic')
 
-	.controller('MeetupDetailCtrl', [function () {
-		console.log('- MeetupDetailCtrl init');
+	.controller('MeetupDetailCtrl', ['$scope', '$routeParams', '$http', function ($scope, $routeParams, $http) {
+        $scope.meetup = {};
+        $scope.feedback = '';
+
+        var meetupId = $routeParams.meetupId;
+
+        $http.get('http://192.168.0.15:8080/meetups/' + meetupId)
+            .success(function (data) {
+                $scope.meetup = data;
+                $scope.feedback = '';
+            })
+            .error(function (error) {
+                $scope.feedback = error;
+            });
 	}]);
