f8009ca0fd5637751997b83710f6eed8f9c72751
Niels Dequeker <niels.dequeker@gmail.com>
2014-04-06 19:43:41 +0200

    Search method for SearchCtrl scope

--- app/js/pages/search/search.js
+++ app/js/pages/search/search.js
@@ -1,13 +1,15 @@
 angular.module('ic')
 
 	.controller('SearchCtrl', ['$scope', '$http', '$log', '$location', 'APIConfig', function ($scope, $http, $log, $location, APIConfig) {
-		$http.get(APIConfig.prefix + '/search/akka')
-			.success(function (response) {
-				$scope.meetups = response.results;
-			})
-			.error(function (message) {
-				$log.info(message);
-			});
+        $scope.search = function (keyword) {
+            $http.get(APIConfig.prefix + '/search/' + keyword)
+                .success(function (response) {
+                    $scope.meetups = response.results;
+                })
+                .error(function (message) {
+                    $log.info(message);
+                });
+        };
 
         $scope.showMeetup = function (meetupId) {
             $log.info('Go to meetup with id ' + meetupId);
