d2e969210f3d3cc6b5defceca7401156688a73b3
Niels Dequeker <niels.dequeker@gmail.com>
2014-05-08 17:52:08 +0200

    Add range directive

---
+++ app/js/utilities.js
@@ -0,0 +1,10 @@
+angular.module('ic')
+
+  .filter('range', function() {
+    return function(input, total) {
+      total = parseInt(total);
+      for (var i=1; i<=total; i++)
+        input.push(i);
+      return input;
+    };
+  });
